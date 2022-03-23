2bdc14b7e2c801f3445b669c073b6f1fc7c8ad69
Danila <visapla000@gmail.com>
2022-03-23 22:09:00 +0500

    Extraction added

---
+++ 9_BigIntegers_DEAD/BigIntegers/Header.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<stdio.h>
+
+char* summ(char*fop, char*sop);
+char* diff(char* fop, char* sop);
+char* mult(char* fop, char* sop);
+char* div(char* fop, char* sop);
+
+inline char* mult(char* fop, char* sop)
+{
+
+}
+
+char* div(char* fop, char* sop)
+{
+	
+}
+
+inline char* diff(char* fop, char* sop)
+{
+
+}
+inline char* summ(char* fop, char* sop)
+{
+	
+}
